1fe6ed3fe85832bc8b12f4269822069b1e46671b
imytuuu <tina1051022@gmail.com>
2025-11-17 01:05:32 +0800

    Refactor constants.asm to streamline definitions
    Removed redundant variable definitions and updated comments.

--- data/constants.asm
+++ data/constants.asm
@@ -1,69 +1,50 @@
 ; ============================================
-; constants.asm
-; 常數定義 - Windows 32-bit
+; constants.asm - 全域常數定義
 ; ============================================
 
 .386
-.model flat, stdcall
 option casemap:none
 
+; ----------------------
+; 不會變動的常數（建議 EQU）
+; ----------------------
+MAX_LIVES       EQU 5
+MAX_LEVEL       EQU 10
+
+SCREEN_WIDTH    EQU 80
+SCREEN_HEIGHT   EQU 25
+
+MAX_ENEMIES     EQU 10
+
+ENEMY_POINTS    EQU 10
+BOSS_POINTS     EQU 100
+BONUS_POINTS    EQU 50
+
+; 遊戲狀態
+STATE_MENU      EQU 0
+STATE_PLAYING   EQU 1
+STATE_PAUSED    EQU 2
+STATE_GAMEOVER  EQU 3
+
+; 方向常數
+DIR_NONE        EQU 0
+DIR_LEFT        EQU 1
+DIR_RIGHT       EQU 2
+DIR_UP          EQU 3
+DIR_DOWN        EQU 4
+
+; ----------------------
+; 可能由遊戲調整的變數（保留 dd）
+; ----------------------
 .data
-    ; 遊戲常數
-    PUBLIC MAX_LIVES
-    PUBLIC MAX_LEVEL
-    PUBLIC SCREEN_WIDTH
-    PUBLIC SCREEN_HEIGHT
-    PUBLIC MAX_ENEMIES
-    PUBLIC INITIAL_SCORE
-    
-    MAX_LIVES       dd 5
-    MAX_LEVEL       dd 10
-    SCREEN_WIDTH    dd 80
-    SCREEN_HEIGHT   dd 25
-    MAX_ENEMIES     dd 10
-    INITIAL_SCORE   dd 0
-    
-    ; 分數常數
-    PUBLIC ENEMY_POINTS
-    PUBLIC BOSS_POINTS
-    PUBLIC BONUS_POINTS
-    
-    ENEMY_POINTS    dd 10
-    BOSS_POINTS     dd 100
-    BONUS_POINTS    dd 50
-    
-    ; 時間常數 (毫秒)
     PUBLIC FRAME_DELAY
     PUBLIC INPUT_DELAY
     PUBLIC ANIMATION_SPEED
+    PUBLIC INITIAL_SCORE
     
-    FRAME_DELAY     dd 33   ; ~30 FPS
+    FRAME_DELAY     dd 33
     INPUT_DELAY     dd 100
     ANIMATION_SPEED dd 200
-    
-    ; 遊戲狀態常數
-    PUBLIC STATE_MENU
-    PUBLIC STATE_PLAYING
-    PUBLIC STATE_PAUSED
-    PUBLIC STATE_GAMEOVER
-    
-    STATE_MENU      dd 0
-    STATE_PLAYING   dd 1
-    STATE_PAUSED    dd 2
-    STATE_GAMEOVER  dd 3
-    
-    ; 方向常數
-    PUBLIC DIR_NONE
-    PUBLIC DIR_LEFT
-    PUBLIC DIR_RIGHT
-    PUBLIC DIR_UP
-    PUBLIC DIR_DOWN
-    
-    DIR_NONE        dd 0
-    DIR_LEFT        dd 1
-    DIR_RIGHT       dd 2
-    DIR_UP          dd 3
-    DIR_DOWN        dd 4
+    INITIAL_SCORE   dd 0
 
-.code
 END
